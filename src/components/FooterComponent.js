c223b14c3d22c57d2be02dd7f8b1496040203360
sahan542 <sahanrashmika542@gmail.com>
2023-10-08 00:00:33 +0530

    Adding header & footer

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span className='text-muted'><h4>All Rights reserved @Sahan</h4></span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent;
